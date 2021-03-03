3aa84f4d33a58d1dc770f8cc6d669772ec18055b
royhan <royhanm23@gmail.com>
2021-03-03 22:09:00 +0700

    integrate pagination, fixing tests add swagger documentation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const PORT = 8010;
 
-// const bodyParser = require('body-parser');
-// const jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 
 const sqlite3 = require('sqlite3').verbose();
 
@@ -12,11 +13,15 @@ const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
 const winston = require('./config/winston');
+const api = require('./src/app')(db);
+const swaggerDocument = require('./config/swagger.json');
 
 db.serialize(() => {
   buildSchemas(db);
 
-  // const app = require('./src/app')(db);
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(jsonParser);
+  app.use(api);
 
   app.listen(PORT, () => {
     winston.info(`App started and listening on PORT ${PORT}`);
